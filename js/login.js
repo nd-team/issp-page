c8fdea5ae76bf7a6540fd1ed672c84ccf62b60ad
lijianhu1 <403056352@qq.com>
2016-12-21 11:21:40 +0800

    login验证

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 /**
  * Created by ike on 2016/12/9.
  */
-    $(function(){
+$(function(){
     $('.log-red').hover(function(){
         $(".login-WeChat",this).toggle();
     });
@@ -14,10 +14,29 @@
     $(this).attr({"id":"checked"+i});
     $(this).next("label").attr("for","checked"+i);
 })
-})
+    $('#form').validate({
+        rules:{
+            username:{
+                required:true,
+            },
+            password:{
+                required:true
+            }
+        },
+        messages:{
+            username:{
+                required:"用户名不能为空！",
+            },
+            password:{
+                required:"密码不能为空！"
+            }
+        }
+    });
+});
     function CountSize(){
     $("LabelInfo").each(function(){
         var LabelInfo=$(this).parent().find("label").outerWidth(true);
         $(this).css("width",$(this).parent().width()-LabelInfo-6+"px");
-    })
+    });
+
 }
