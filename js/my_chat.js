511ac5a1d945882b54853a00d14b27534f63adc7
daixiuhua <1293225334@qq.com>
2017-01-05 15:53:38 +0800

    modified chatroom pages all style

--- js/my_chat.js
+++ js/my_chat.js
@@ -6,9 +6,10 @@ $(function(){
     $('.close').on('click', '.close_module', function(){
         $('.module').hide();
         $('.module-two').hide();
+        $('.module-next').hide();
     });
     $('.add-people').on('click', '.added', function(){
-        $('.module-two').show();
+        $('.module-next').show();
         $('.module').hide();
     });
     $('.talk-news').on('click', '.talk', function(){
@@ -25,8 +26,10 @@ $(function(){
     $('.same-circle').on('click',function(){
         if(!$(this).hasClass('circle')){
             $(this).addClass('circle').children('svg').show();
+            $(this).parents('.all-member').addClass('current');
         }else{
             $(this).removeClass('circle').children('svg').hide();
+            $(this).parents('.all-member').removeClass('current');
         }
     });
 });
