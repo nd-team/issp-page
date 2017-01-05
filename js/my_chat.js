04f4dfa7d4b834cd241b9b117f4f61e4576cc0d3
daixiuhua <1293225334@qq.com>
2017-01-05 14:32:07 +0800

    do chat pages special effects

--- js/my_chat.js
+++ js/my_chat.js
@@ -6,12 +6,27 @@ $(function(){
     $('.close').on('click', '.close_module', function(){
         $('.module').hide();
         $('.module-two').hide();
-    })
+    });
     $('.add-people').on('click', '.added', function(){
         $('.module-two').show();
         $('.module').hide();
-    })
+    });
     $('.talk-news').on('click', '.talk', function(){
         $('.module-two').show();
-    })
+    });
+
+    $(".my-friends").on('click', '.good-friends' ,function() {
+        $(".my-friends-list").slideToggle();
+    });
+    $(".group-friends").on('click', '.good-friends' ,function() {
+        $(".group-friends-list").slideToggle();
+    });
+
+    $('.same-circle').on('click',function(){
+        if(!$(this).hasClass('circle')){
+            $(this).addClass('circle').children('svg').show();
+        }else{
+            $(this).removeClass('circle').children('svg').hide();
+        }
+    });
 });
