81e48d1af853c59c745faa2a2c6a74a9a10a7c13
lijianhu1 <403056352@qq.com>
2017-01-18 15:24:12 +0800

    兼容

--- js/activity_index.js
+++ js/activity_index.js
@@ -8,17 +8,17 @@ $(function(){
         slidesNavigation:true,
         navigationTooltips: ['首页', '惩罚圈', '活动圈', '社交圈', '众筹圈', '夺宝圈', '分享圈']
     });
-    var time = setInterval(function(){
-        $.fn.fullpage.moveSlideRight();
-    }, 3000);
-    $('.search-bar').on({
-        "focus":function(){
-            clearInterval(time)
-        },
-        "blur":function(){
-            setInterval(function(){
-                $.fn.fullpage.moveSlideRight();
-            }, 3000);
-        }
-    },"input");
+    // var time = setInterval(function(){
+    //     $.fn.fullpage.moveSlideRight();
+    // }, 3000);
+    // $('.search-bar').on({
+    //     "focus":function(){
+    //         clearInterval(time)
+    //     },
+    //     "blur":function(){
+    //         setInterval(function(){
+    //             $.fn.fullpage.moveSlideRight();
+    //         }, 3000);
+    //     }
+    // },"input");
 });
