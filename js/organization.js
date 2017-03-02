d8ccda6dc088e2824904bf0c2c1244c71e958afb
daixiuhua <1293225334@qq.com>
2017-03-02 15:16:24 +0800

    modify the system page the style

--- js/organization.js
+++ js/organization.js
@@ -3,4 +3,26 @@ $(function(){
     $(".header-menu").on('click',function() {
         $(".menu").slideToggle();
     });
+
+    (function($){
+        $.fn.myTab = function(options){//将对象作为参数传进去
+            var defult = {parent:"",index:null},
+                setting =$.extend(defult,options);//extend方法合并两个对象，如果两个对象有相同属性，则后面的那个对象的属性覆盖前面那个对象的属性。
+            $(this).find("li").on("click",function(){
+                var $this=$(this),
+                    $index =$this.index();
+                $this.addClass("current").siblings().removeClass("current");
+                $(setting.parent).children("ul").eq($index).show().siblings().hide();//切换标签对应的内容
+
+            })
+            //判断进入页面显示那个标签样式和内容
+            if(setting.index==null){
+                $(this).find('li').eq(0).click();
+            }else{
+                $(this).find('li').eq(options.index).click();
+            }
+        }
+    })(jQuery);
+
+    $(".list").myTab({parent:".content-tab",index:0});
 });
