c2a50fd9c466d5a4d77a370df49b1e7539a93397
daixiuhua <1293225334@qq.com>
2017-03-09 15:48:34 +0800

    modified the organization job page and the style

--- js/organization.js
+++ js/organization.js
@@ -1,7 +1,24 @@
 $(function(){
 
-    $(".header-menu").on('click',function() {
-        $(".menu").slideToggle();
+    $(".see-children").on('click','span',function() {
+        var inx = $(this).index(),
+            selfText = $(this).text(),
+            num = selfText.length,
+            width = $(this).width(),
+            titleText = $(this).parent().siblings().children().eq(inx).text(),
+            fontSize = $(this).css("font-size").split("p")[0]*num;
+            console.log(fontSize,width);
+        if (fontSize>width){
+            $(".module").show();
+            $(".module .see-type").text(titleText);
+            $(".module .see-description").text(selfText);
+        }else {
+            return
+        }
+    });
+
+    $(".module .see-module").on('click','.btn',function() {
+        $(".module ").hide();
     });
 
     (function($){
