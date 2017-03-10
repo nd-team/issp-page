589f3be6b2752c83b4ab96fb8c2d12deae861ad3
daixiuhua <1293225334@qq.com>
2017-03-10 16:42:51 +0800

    modified the page and style

--- js/new_modules/common.js
+++ js/new_modules/common.js
@@ -36,9 +36,27 @@ $(document).ready(function(){
         $(this).parent().parent().hide()
     })
 
+    // click more slide
+    $(".see-children").on('click','span',function() {
+        var inx = $(this).index(),
+            selfText = $(this).text(),
+            num = selfText.length,
+            width = $(this).width(),
+            titleText = $(this).parent().siblings().children().eq(inx).text(),
+            fontSize = $(this).css("font-size").split("p")[0]*num;
+            console.log(fontSize,width);
+        if (fontSize>width){
+            $(".module").show();
+            $(".module .see-type").text(titleText);
+            $(".module .see-description").text(selfText);
+        }else {
+            return
+        }
+    });
 
-
-
+    $(".module .see-module").on('click','.btn',function() {
+        $(".module ").hide();
+    });
     //切换菜单栏
     $('.top-fixed .list').on("click","li",function(){
         var listTitle = $(this).attr("data-title");
